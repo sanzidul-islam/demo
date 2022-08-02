f542153c81c03a19bb4cb8ca96f0a3b093308214
sanzidul-islam <sanzidulislam16@gmail.com>
2022-08-03 01:03:47 +0600

    1st

---
+++ DAL/Repos/UserDetails/UserRepo.cs
@@ -0,0 +1,38 @@
+﻿using DAL.EF;
+using DAL.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos.UserDetails
+{
+    internal class UserRepo : IRepo<user_details, int, bool>
+    {
+        public bool Create(user_details obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<user_details> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public user_details Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(user_details obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/interfaces/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.interfaces
+{
+    internal interface IRepo<CLASS, ID, RT>
+    {
+        List<CLASS> Get();
+        CLASS Get(ID id);
+        RT Create(CLASS obj);
+        bool Update(CLASS obj);
+        bool Delete(ID id);
+    }
+}
